4db4d8b0181203f17aa9c45600058408999911b1
Pisut Theeraphanphichate <pisut.flyinggiraffe@gmail.com>
2023-01-23 17:34:05 +0700

    add register validator

--- src/features/auth/RegisterContainer.js
+++ src/features/auth/RegisterContainer.js
@@ -1,31 +1,23 @@
+import { useState } from 'react';
+import Modal from '../../components/Modal';
 import RegisterForm from './RegisterForm';
 
 export default function RegisterContainer() {
+  const [open, setOpen] = useState(false);
   return (
-    <div className="text-center tw-py-2.5">
-      <button className="btn fw-bold btn-green rounded-md h-12" type="button">
-        Create New Account
-      </button>
-
-      {/* Begin Modal */}
-
-      <div className="modal fade show d-block" tabIndex="-1">
-        <div className="modal-dialog modal-dialog-centered">
-          <div className="modal-content">
-            <div className="modal-header">
-              <button type="button" className="btn-close invisible" />
-              <h5 className="modal-title">Sign Up</h5>
-              <button type="button" className="btn-close" />
-            </div>
-            <div className="modal-body">
-              <RegisterForm />
-            </div>
-          </div>
-        </div>
+    <>
+      <div className="text-center tw-py-2.5">
+        <button
+          className="btn fw-bold btn-green rounded-md h-12"
+          type="button"
+          onClick={() => setOpen(true)}
+        >
+          Create New Account
+        </button>
       </div>
-      <div className="modal-backdrop fade show" />
-
-      {/* End Modal */}
-    </div>
+      <Modal open={open} onClose={() => setOpen(false)} title="Sign Up">
+        <RegisterForm />
+      </Modal>
+    </>
   );
 }
--- src/features/auth/RegisterForm.js
+++ src/features/auth/RegisterForm.js
@@ -1,31 +1,124 @@
+import { useState } from 'react';
+// import { object, string, ref } from 'yup';
+import * as Joi from 'joi';
+
+const initialInput = {
+  firstName: '',
+  lastName: '',
+  emailOrMobile: '',
+  password: '',
+  confirmPassword: ''
+};
+
+const registerSchema = Joi.object({
+  firstName: Joi.string().trim().required(),
+  lastName: Joi.string().trim().required(),
+  emailOrMobile: Joi.alternatives().try(
+    Joi.string().email({ tlds: false }),
+    Joi.string().pattern(/^[0-9]{10}$/)
+  ),
+  password: Joi.string().alphanum().min(6).required().trim(),
+  confirmPassword: Joi.string().valid(Joi.ref('password')).required().trim()
+});
+
+const validateRegister = input => {
+  return registerSchema.validate(input, {
+    abortEarly: false
+  });
+};
+
+// const registerSchema = object({
+//   firstName: string().trim().required(),
+//   lastName: string().trim().required(),
+//   password: string()
+//     .matches(/^[0-9a-zA-Z]{6,}$/)
+//     .required(),
+//   confirmPassword: ref('password').required()
+// });
+
+// const validateRegister = async input => {
+//   try {
+//     const value = await registerSchema.validate(input, {
+//       abortEarly: false,
+//       stripUnknown: true
+//     });
+
+//   } catch (err) {}
+// };
+
 export default function RegisterForm() {
+  const [input, setInput] = useState(initialInput);
+  const [error, setError] = useState(initialInput);
+
+  const handleChangeInput = e => {
+    setInput({ ...input, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmitForm = e => {
+    e.preventDefault();
+    const { value, error } = validateRegister(input);
+    if (error) {
+      const newError = error.details.reduce((acc, el) => {
+        acc[el.context.label] = el.message;
+        return acc;
+      }, {});
+      setError(newError);
+    }
+    console.dir(error);
+  };
+
   return (
-    <form className="row gx-2 gy-3">
+    <form className="row gx-2 gy-3" onSubmit={handleSubmitForm}>
       <div className="col-6">
-        <input className="form-control" type="text" placeholder="First name" />
+        <input
+          className="form-control is-invalid"
+          type="text"
+          placeholder="First name"
+          name="firstName"
+          value={input.firstName}
+          onChange={handleChangeInput}
+        />
+        <div className="invalid-feedback">{error.firstName}</div>
       </div>
       <div className="col-6">
-        <input className="form-control" type="text" placeholder="Last name" />
+        <input
+          className="form-control is-invalid"
+          type="text"
+          placeholder="Last name"
+          name="lastName"
+          value={input.lastName}
+          onChange={handleChangeInput}
+        />
+        <div className="invalid-feedback">{error.lastName}</div>
       </div>
       <div className="col-12">
         <input
           className="form-control"
           type="text"
           placeholder="Mobile number or email address"
+          name="emailOrMobile"
+          value={input.emailOrMobile}
+          onChange={handleChangeInput}
         />
       </div>
       <div className="col-12">
         <input
           className="form-control"
-          type="text"
+          type="password"
           placeholder="New password"
+          name="password"
+          value={input.password}
+          onChange={handleChangeInput}
         />
       </div>
       <div className="col-12">
         <input
           className="form-control"
-          type="text"
+          type="password"
           placeholder="Confirm password"
+          name="confirmPassword"
+          value={input.confirmPassword}
+          onChange={handleChangeInput}
         />
       </div>
       <div className="col-12">
