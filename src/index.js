7812b1a2140f92351779acb2b14755e75c1accc7
Pisut Theeraphanphichate <pisut.flyinggiraffe@gmail.com>
2023-01-23 15:26:16 +0700

    auth ui

--- src/index.js
+++ src/index.js
@@ -4,10 +4,13 @@ import '@fortawesome/fontawesome-free/css/all.min.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
+import AuthContextProvider from './contexts/AuthContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <AuthContextProvider>
+      <App />
+    </AuthContextProvider>
   </React.StrictMode>
 );
