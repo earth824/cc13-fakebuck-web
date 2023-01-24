511fb33839b25b1caf639a35c369883b4a414acd
Pisut Theeraphanphichate <pisut.flyinggiraffe@gmail.com>
2023-01-24 15:42:23 +0700

    feature: register

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import { ToastContainer } from 'react-toastify';
+import Spinner from './components/Spinner';
+import useLoading from './hooks/useLoading';
+
 import Router from './routes/Router';
 
 export default function App() {
-  return <Router />;
+  const { loading } = useLoading();
+  return (
+    <>
+      {loading && <Spinner />}
+      <Router />
+      <ToastContainer autoClose="1000" theme="light" position="bottom-center" />
+    </>
+  );
 }
