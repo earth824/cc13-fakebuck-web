59f126a3b202f704cdfeebf527cc7cd71ff07452
Pisut Theeraphanphichate <pisut.flyinggiraffe@gmail.com>
2023-01-24 10:24:19 +0700

    add register validator

--- src/features/auth/RegisterForm.js
+++ src/features/auth/RegisterForm.js
@@ -1,6 +1,6 @@
 import { useState } from 'react';
-// import { object, string, ref } from 'yup';
-import * as Joi from 'joi';
+import Input from '../../components/Input';
+import validateRegister from '../../validators/validate-register';
 
 const initialInput = {
   firstName: '',
@@ -10,45 +10,9 @@ const initialInput = {
   confirmPassword: ''
 };
 
-const registerSchema = Joi.object({
-  firstName: Joi.string().trim().required(),
-  lastName: Joi.string().trim().required(),
-  emailOrMobile: Joi.alternatives().try(
-    Joi.string().email({ tlds: false }),
-    Joi.string().pattern(/^[0-9]{10}$/)
-  ),
-  password: Joi.string().alphanum().min(6).required().trim(),
-  confirmPassword: Joi.string().valid(Joi.ref('password')).required().trim()
-});
-
-const validateRegister = input => {
-  return registerSchema.validate(input, {
-    abortEarly: false
-  });
-};
-
-// const registerSchema = object({
-//   firstName: string().trim().required(),
-//   lastName: string().trim().required(),
-//   password: string()
-//     .matches(/^[0-9a-zA-Z]{6,}$/)
-//     .required(),
-//   confirmPassword: ref('password').required()
-// });
-
-// const validateRegister = async input => {
-//   try {
-//     const value = await registerSchema.validate(input, {
-//       abortEarly: false,
-//       stripUnknown: true
-//     });
-
-//   } catch (err) {}
-// };
-
 export default function RegisterForm() {
   const [input, setInput] = useState(initialInput);
-  const [error, setError] = useState(initialInput);
+  const [error, setError] = useState({});
 
   const handleChangeInput = e => {
     setInput({ ...input, [e.target.name]: e.target.value });
@@ -56,69 +20,61 @@ export default function RegisterForm() {
 
   const handleSubmitForm = e => {
     e.preventDefault();
-    const { value, error } = validateRegister(input);
-    if (error) {
-      const newError = error.details.reduce((acc, el) => {
-        acc[el.context.label] = el.message;
-        return acc;
-      }, {});
-      setError(newError);
+    const result = validateRegister(input);
+    if (result) {
+      setError(result);
+    } else {
+      setError({});
     }
-    console.dir(error);
   };
 
   return (
     <form className="row gx-2 gy-3" onSubmit={handleSubmitForm}>
       <div className="col-6">
-        <input
-          className="form-control is-invalid"
-          type="text"
+        <Input
           placeholder="First name"
           name="firstName"
           value={input.firstName}
           onChange={handleChangeInput}
+          error={error.firstName}
         />
-        <div className="invalid-feedback">{error.firstName}</div>
       </div>
       <div className="col-6">
-        <input
-          className="form-control is-invalid"
-          type="text"
+        <Input
           placeholder="Last name"
           name="lastName"
           value={input.lastName}
           onChange={handleChangeInput}
+          error={error.lastName}
         />
-        <div className="invalid-feedback">{error.lastName}</div>
       </div>
       <div className="col-12">
-        <input
-          className="form-control"
-          type="text"
-          placeholder="Mobile number or email address"
+        <Input
+          placeholder="Email address or mobile number"
           name="emailOrMobile"
           value={input.emailOrMobile}
           onChange={handleChangeInput}
+          error={error.emailOrMobile}
         />
       </div>
       <div className="col-12">
-        <input
-          className="form-control"
+        <Input
           type="password"
           placeholder="New password"
           name="password"
           value={input.password}
           onChange={handleChangeInput}
+          error={error.password}
         />
       </div>
       <div className="col-12">
-        <input
-          className="form-control"
+        <Input
           type="password"
           placeholder="Confirm password"
           name="confirmPassword"
           value={input.confirmPassword}
           onChange={handleChangeInput}
+          error={error.confirmPassword}
         />
       </div>
       <div className="col-12">
